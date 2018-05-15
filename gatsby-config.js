df6ce36f0114ec60862f60bab63e43e7da19f58d
Julie Chevalier <julessaysrawr@users.noreply.github.com>
2018-05-15 17:45:28 -0400

    removing dead code

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,15 +31,3 @@ module.exports = {
     },  
   ],
 }
-
-// plugins: [
-//   {
-//     resolve: `gatsby-source-filesystem`,    // this sources the files from the filesystem
-//     options: {
-//       path: `${__dirname}/src/pages`,
-//       name: "markdown-pages",
-//     },
-//   },
-//   `gatsby-transformer-remark`,    // “transform” the markdown to HTML and the YAML frontmatter to JSON
-// ];
-
