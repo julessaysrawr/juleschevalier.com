26f0a4eeef352d2746369f5432bccd35645ca7d5
Julie Chevalier <julessaysrawr@users.noreply.github.com>
2018-05-25 13:12:54 -0400

    Spell check and adding emojis 😋

--- src/components/now.js
+++ src/components/now.js
@@ -5,13 +5,15 @@ import '../styles/queries.css'
 const Now = props => {
   return (
     <div>
-      <h3 className="space-below">What I'm doing now</h3>
+      <h2 className="space-below">What I'm doing now</h2>
       <p>Letting go and finding I am happier with less</p>
       <p>Trying to live in the present moment</p>
-      <p>Becoming a morning person</p>
-      <p>Employed in software engineering</p>
-      <p>Experimenting with 75% plant based diet.  Keto-ish.</p>
-      <p className="space-above">Updated on: January 27, 2018</p>
+      <p>Focusing on creating: 🖋wrting and 📷photography</p>
+      <p>Playing Elder Scrolls Online on XBox, in my ugly recliner 🎮</p>
+      <p>Learning that doing nothing is a productivity technique</p>
+      <p>Eating whole foods 🥗🥒🍅🥕🍠  Keto-ish🍖🍳🥓</p>
+      <p>Employed in software engineering </p>
+      <p className="space-above">Updated on: May 25, 2018</p>
     </div>
   );
 };
