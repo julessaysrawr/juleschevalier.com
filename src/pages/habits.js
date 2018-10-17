7caf757eb97fe3a0e391acdecfe07d8a99eb9f29
Julie Chevalier <julessaysrawr@users.noreply.github.com>
2018-10-16 20:56:48 -0400

    drying up the code (thanks paul!)

--- src/pages/habits.js
+++ src/pages/habits.js
@@ -8,10 +8,10 @@ class Habits extends Component {
 
   constructor(props) {
     super(props);
-    this.createHandler = this.createHandler.bind(this);
-    this.relaxHandler = this.relaxHandler.bind(this);
-    this.strongBodyHandler = this.strongBodyHandler.bind(this);
-    this.clearMindHandler = this.clearMindHandler.bind(this);
+    // this.createHandler = this.createHandler.bind(this);
+    // this.relaxHandler = this.relaxHandler.bind(this);
+    // this.strongBodyHandler = this.strongBodyHandler.bind(this);
+    // this.clearMindHandler = this.clearMindHandler.bind(this);
 
     // this.randomHandler = this.randomHandler.bind(this);
 
@@ -22,7 +22,7 @@ class Habits extends Component {
       create: ["writing", "photography", "editing", "coding", "learning"],
       relax: ["do nothing", "get bored", "cooking", "friends", "read fiction", "pamper myself"],
       strongBody: ["stretches", "yoga", "body weight movements", "weight lifting", "walking", "handstand progress"],
-      clearMind: ["nap", "meditate", "presence", "video games", "reading", "sunshine", "walking"]
+      clearMind: ["nap", "meditate", "presence", "video games", "reading", "sunshine", "walking", "do nothing"]
     }
   }
 
@@ -31,35 +31,10 @@ class Habits extends Component {
     return obj[keys[Math.floor(Math.random() * keys.length)]];
   }
 
-  // randomHandler(state) {
-  //   let randomItem = this.randomObject(state);
-  //   console.log("randomItem: ", randomItem);
-  // }
 
-  createHandler() {
-    let randomItem = this.randomObject(this.state.create);
+  randomHandler = (habit) => {
+    let randomItem = this.randomObject(this.state[habit]);
     this.setState({result: randomItem});
-    console.log("randomItem: ", randomItem);
-  }
-
-  relaxHandler() {
-    let randomItem = this.randomObject(this.state.relax);
-    this.setState({result: randomItem});
-    console.log("randomItem: ", randomItem);
-  }
-
-  strongBodyHandler() {
-    let randomItem = this.randomObject(this.state.strongBody);
-    this.setState({result: randomItem});
-    console.log("randomItem: ", randomItem);
-  }
-
-  clearMindHandler() {
-    let randomItem = this.randomObject(this.state.clearMind);
-    this.setState({result: randomItem});
-    // console.log("result: ", this.state.result);
-    
-    console.log("randomItem: ", randomItem);
   }
 
   render() {
@@ -69,11 +44,11 @@ class Habits extends Component {
         <header>
           <Nav />
         </header>
-        <div className="flex-container space-around">
-          <button onClick={this.createHandler} className="button">create</button>
-          <button onClick={this.relaxHandler} className="button">relax</button>
-          <button onClick={this.strongBodyHandler} className="button">strong body</button>
-          <button onClick={this.clearMindHandler} className="button">clear mind</button>
+        <div className="flex-container space-around wrap">
+          <button onClick={() => this.randomHandler('create')} className="button">create</button>
+          <button onClick={() => this.randomHandler('relax')} className="button">relax</button>
+          <button onClick={() => this.randomHandler('strongBody')} className="button">strong body</button>
+          <button onClick={() => this.randomHandler('clearMind')} className="button">clear mind</button>
           
         </div>
         <div className="flex-container">
