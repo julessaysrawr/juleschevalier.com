7af20a98c80077e4118f44e12aff8b14980d1ffb
Julie Chevalier <julessaysrawr@users.noreply.github.com>
2018-10-18 21:48:34 -0400

    upgraded gatsby to v2 and got scss working

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Link from "gatsby-link";
+import { Link } from "gatsby";
 import '../styles/main.css'
 import '../styles/queries.css'
 import bioPic from '../assets/jules.jpg'
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import '../styles/main.css'
 import '../styles/queries.css'
 import cover from '../assets/peonies.jpg'
 import Nav from '../components/nav.js'
+import '../styles/main.scss'
 
 export default () => 
 <div>
