7bfa50df6d612af0028b96f52187ab1fe7149b0d
Julie Chevalier <julessaysrawr@users.noreply.github.com>
2019-05-27 10:41:44 -0400

    tweaking spacing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import '../styles/main.scss'
+import Nav from '../components/nav.js'
 import Emoji from '../components/emoji'
 
 export default () => (
   <div>
+    <Nav />
     <main>
-      <div className="flex-container space-below-m">
+      <div className="flex-container space-above-m space-below-m">
         <h1>
           Whoops something went wrong <Emoji symbol="😬" label="" />
         </h1>
