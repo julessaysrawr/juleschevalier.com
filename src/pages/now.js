14ffb447bc9f64dc7ef68406a92f0d28b3b44653
Jules Chevalier <julessaysrawr@gmail.com>
2020-07-12 21:40:39 -0400

    feat: implementing new design + updating content

--- src/pages/now.js
+++ src/pages/now.js
@@ -1,49 +1,128 @@
 import React from 'react'
-import '../styles/main.scss'
 import LayoutMain from '../components/layout-main'
+import { css } from '@emotion/core'
+import theme from '../theme'
+import paper from '../images/lightpaperfibers_@2x.png'
 import Emoji from '../components/emoji'
 import Link from '../components/link'
+import GetInTouch from '../components/get-in-touch'
+import nowImage from '../images/Jules-and-Grievy.png'
 
 const Now = () => (
   <LayoutMain
     title={`What I'm Doing Now | Jules Chevalier`}
     description={`What I'm currently up to. Now page for Jules Chevalier`}
   >
-    <main className="flex-container">
-      <div>
-        <h3 className="space-below">What I'm doing now</h3>
-        <p>
-          Working towards a redesign of this site with this{' '}
-          <Link href="dchevalier.io">talented human</Link>.
-        </p>
-        <p>
-          Trying to stay healthy and sane during the pandemic. Grateful for home gym equipment,
-          sunshine and video games.
-        </p>
-        {/* <p>
-          Focusing my creative time on <Emoji symbol=" 🖋 " label="" /> writing.
-        </p> */}
-        <p>Trying to observe my thoughts and detach from them when possible.</p>
-        {/* <p>
+    <main>
+      <div
+        css={css`
+          background-image: url(${paper});
+          background-repeat: repeat;
+          // padding-top: ${theme.space[7]}px;
+          padding-top: 214px;
+          border-bottom: 2px solid rgba(136, 128, 113, 0.25);
+          height: 860px;
+          // height: 214px;
+        `}
+      ></div>
+      <div
+        css={css`
+          background-color: ${theme.color.white};
+          max-width: 1083px;
+          margin: -646px auto calc(${theme.space[2]}px*13);  // if height of paper is 860, and the white starts 214px down, need negative margin of 646px
+          // margin-bottom: calc(${theme.space[2]}px*13);
+          border: 2px solid rgba(136, 128, 113, 0.25);
+          padding: 88px 109px;
+        `}
+      >
+        <img
+          src={nowImage}
+          alt=""
+          css={css`
+            width: 865px;
+            height: 468px;
+            margin: 0 auto;
+            border: 2px solid rgba(136, 128, 113, 0.25);
+            object-fit: cover;
+            margin-bottom: 80px;
+          `}
+        />
+        <div
+          css={css`
+            max-width: ${theme.contentWidths.copyWidth}px;
+            margin: 0 auto;
+          `}
+        >
+          <div
+            css={css`
+              margin-bottom: 56px;
+            `}
+          >
+            <h1
+              css={css`
+                margin-bottom: ${theme.space[4]}px;
+              `}
+            >
+              What I&apos;m doing now
+            </h1>
+            <p
+              css={css`
+                font-weight: 400;
+              `}
+            >
+              Updated on: July 12th, 2020
+            </p>
+
+            <ul>
+              <li>
+                Refreshing this site thanks to the design effort of this{' '}
+                <Link type="inline" href="dchevalier.io">
+                  talented human
+                </Link>
+                .
+              </li>
+              <li>
+                Grateful for purchasing an exercise bike recently. Doing cardio is what is giving me
+                the motivation to strength train (my favorite type of movement that brings me joy).
+              </li>
+              <li>
+                Black Lives Matter: Learning how to practice allyship. Figuring out what my
+                anti-racism lane looks like.
+              </li>
+              <li>
+                Bringing in rituals as opportunities for intention setting. New & full moon
+                ceremonies for now.
+              </li>
+            </ul>
+          </div>
+
+          {/* <p>
           Grateful to be a part of the team behind:{' '}
           <Link type={'basic'} href="https://be-ingoodcompany.com/">
             In Good Company
           </Link>
         </p> */}
-        <p className="space-above">Updated on: May 9, 2020</p>
-
-        <p className="space-above-m">
-          (thanks to{' '}
-          <Link type={'basic'} href="https://sivers.org/">
-            Derek Sivers
-          </Link>{' '}
-          for this{' '}
-          <Link type={'basic'} href="https://nownownow.com/about">
-            idea
+          <Link
+            type="inline"
+            hrefLocal="/about"
+            description="Learn more about me by going to the about page"
+          >
+            Learn More About Me &gt;
           </Link>
-          )
-        </p>
+
+          {/* <p>
+            Thanks to{' '}
+            <Link type={'basic'} href="https://sivers.org/">
+              Derek Sivers
+            </Link>{' '}
+            for the{' '}
+            <Link type={'basic'} href="https://nownownow.com/about">
+              now page idea
+            </Link>
+          </p> */}
+        </div>
       </div>
+      <GetInTouch />
     </main>
   </LayoutMain>
 )
