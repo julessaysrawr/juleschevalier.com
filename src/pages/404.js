7ea4fa0a86b089a6c85576414f44e1d448ad7122
Jules Chevalier <julessaysrawr@gmail.com>
2020-07-12 23:14:03 -0400

    feat: implementing design changes to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,45 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import '../styles/main.scss'
+import { css } from '@emotion/core'
+import theme from '../theme'
+import paper from '../images/lightpaperfibers_@2x.png'
 import LayoutMain from '../components/layout-main'
+import Link from '../components/link'
 import Emoji from '../components/emoji'
+import image404 from '../images/404-Error-Cute.svg'
 
 export default () => (
   <LayoutMain>
     <main>
-      <div className="flex-container space-above-m space-below-m">
-        <h1>
-          Whoops something went wrong <Emoji symbol="😬" label="" />
-        </h1>
-      </div>
-      <div className="flex-container space-above-m">
-        <Link to="/" className="show-link">
-          Go back
-        </Link>
+      <div
+        css={css`
+          background-image: url(${paper});
+          background-repeat: repeat;
+          // height: 100%;
+          // padding-top: 100px;
+          padding-top: ${theme.space[6]}px;
+          border-bottom: 2px solid rgba(136, 128, 113, 0.25);
+        `}
+      >
+        <div
+          css={css`
+            max-width: ${theme.contentWidths.copyWidth}px;
+            padding-bottom: 100px;
+            margin: 0 auto;
+            text-align: center;
+          `}
+        >
+          <img
+            src={image404}
+            alt="Illustration of cat hanging inside the 0 of 404"
+            css={css`
+              width: ${theme.contentWidths.copyWidth}px;
+              margin: 0 auto;
+            `}
+          />
+          <Link type="buttonVariableWidth" hrefLocal="/">
+            Return To Homepage
+          </Link>
+        </div>
       </div>
     </main>
   </LayoutMain>
