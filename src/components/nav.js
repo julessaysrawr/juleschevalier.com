07408ce9a243f7b487b6cab012f01c533e070996
Jules Chevalier <julessaysrawr@gmail.com>
2020-08-03 08:34:50 -0400

    feat: adding mobile nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { css } from '@emotion/core'
 import theme from '../theme'
 import { Link } from 'gatsby'
@@ -7,8 +7,11 @@ import IconEmail from './social-links/email'
 import IconTwitter from './social-links/twitter'
 import IconGithub from './social-links/github'
 import IconLinkedIn from './social-links/linkedin'
+import { bpTabletLG } from '../lib/breakpoints'
 
 const Nav = () => {
+  const [open, setOpen] = useState(false)
+
   const pageLink = css`
     text-decoration: none;
     // margin: ${theme.space[2]}px ${theme.space[4]}px 0;
@@ -23,6 +26,18 @@ const Nav = () => {
     &:hover {
       text-decoration: underline;
     }
+
+    ${bpTabletLG} {
+      // text-align: center;
+      // margin: 0 auto;
+      font-size: ${theme.textSizes.h1Mobile};
+
+      ${
+        open === true
+          ? 'display: flex; justify-content: center; text-align: center; margin: 0 auto;'
+          : ''
+      }
+    }
   `
 
   return (
@@ -33,15 +48,19 @@ const Nav = () => {
         padding: ${theme.space[6]}px;
         position: absolute;
         top: 0;
-        // left: 22%;
-        // left: 50%;
-        // margin-left: 656px;
-        // right: 50%;
-        width: 900px;
-        width: 1440px;
 
+        // width: 1440px;
+        width: 100%;
         max-width: 1440px;
-        // margin: 0 auto;
+
+        ${bpTabletLG} {
+
+          padding: ${theme.space[5]}px ${theme.space[4]}px;
+        }
+
+        ${open === true
+          ? 'position: absolute; top: 0; left: 0; background-color: white; height: 100vh; width: 100vw; flex-direction: column; z-index: 1;'
+          : ''}
       `}
     >
       <Link
@@ -68,6 +87,14 @@ const Nav = () => {
         css={css`
           display: flex;
           // margin: 0 336px;
+
+          ${bpTabletLG} {
+            display: none;
+
+            ${open === true
+              ? 'display: flex; flex-direction: column; justify-content: center;'
+              : ''}
+          }
         `}
       >
         <Link to="/articles" css={pageLink}>
@@ -88,6 +115,14 @@ const Nav = () => {
           padding-top: calc(${theme.space[2]}px*1.5);
           padding-top: 15px;
           // padding-top: 13px;
+
+          ${bpTabletLG} {
+            display: none;
+
+            ${open === true
+              ? 'display: flex; justify-content: space-between; margin: 0 auto; align-self: flex-end;'
+              : ''}
+          }
         `}
       >
         <IconEmail url={'mailto:hellothere@juleschevalier.com'} />
@@ -95,6 +130,72 @@ const Nav = () => {
         <IconTwitter url={'https://twitter.com/julessaysrawr'} />
         <IconGithub />
         <IconLinkedIn url={'https://www.linkedin.com/in/jules-chevalier-35587337'} />
+      </div>
+      <div
+        // MOBILE NAV ICON
+        css={css`
+          display: none;
+
+          ${bpTabletLG} {
+            display: block;
+            position: absolute;
+            // background: #8e44ad;
+            padding: 10px;
+            height: 40px;
+            cursor: pointer;
+            right: 16px;
+          }
+        `}
+        onClick={() => setOpen(!open)}
+      >
+        <div
+          css={css`
+            width: 35px;
+            height: 3px;
+            background-color: ${theme.color.black};
+            margin: 6px 0;
+            transition: 0.4s;
+            border-radius: 5px;
+
+            // if menu is open
+            // -webkit-transform: rotate(-45deg) translate(-9px, 5px);
+            // transform: rotate(-45deg) translate(-9px, 5px);
+            ${open === true
+              ? '-webkit-transform: rotate(-45deg) translate(-9px, 5px); transform: rotate(-45deg) translate(-9px, 5px);'
+              : ''}
+          `}
+        />
+        <div
+          css={css`
+            width: 35px;
+            height: 3px;
+            background-color: ${theme.color.black};
+            margin: 6px 0;
+            transition: 0.4s;
+            border-radius: 5px;
+
+            // if menu is open
+            // opacity: 0;
+            ${open === true ? 'opacity: 0;' : ''}
+          `}
+        />
+        <div
+          css={css`
+            width: 35px;
+            height: 3px;
+            background-color: ${theme.color.black};
+            margin: 6px 0;
+            transition: 0.4s;
+            border-radius: 5px;
+
+            // if menu is open
+            // -webkit-transform: rotate(45deg) translate(-8px, -4px);
+            // transform: rotate(45deg) translate(-8px, -4px);
+            ${open === true
+              ? '-webkit-transform: rotate(45deg) translate(-8px, -4px); transform: rotate(45deg) translate(-8px, -4px);'
+              : ''}
+          `}
+        />
       </div>
     </div>
   )
