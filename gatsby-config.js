33db6f0df0bb3dbee0170892f7c586ed4d0de15f
Jules Chevalier <julessaysrawr@gmail.com>
2020-08-18 20:35:29 -0400

    feat: adding new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     author: `Jules Chevalier`,
-    title: `Jules Chevalier | Designer, Engineer, Growth Catalyst`,
+    title: `Jules Chevalier | Designer, Engineer & Growth Catalyst`,
     description: `Jules Chevalier is a designer & engineer who writes about emotional wellness, intentional living and inclusion.`,
     siteUrl: `https://juleschevalier.com/`,
     thumbnail: `https://juleschevalier.com/thumbnail.png`
@@ -14,13 +14,13 @@ module.exports = {
         name: 'content'
       }
     },
-    {
-      resolve: `gatsby-source-filesystem`, // this sources the files from the filesystem
-      options: {
-        name: 'lightbox',
-        path: `${__dirname}/src/images/photography` //TODO: revist this when reintroducing lightbox
-      }
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`, // this sources the files from the filesystem
+    //   options: {
+    //     name: 'lightbox',
+    //     path: `${__dirname}/src/images/photography` //TODO: revist this when reintroducing lightbox
+    //   }
+    // },
     {
       resolve: `gatsby-source-filesystem`, // this sources the files from the filesystem
       options: {
@@ -72,6 +72,25 @@ module.exports = {
         policy: [{ userAgent: '*', disallow: ['/topics', '/topics/', '/read', '/read/'] }]
       }
     },
-    `gatsby-plugin-emotion`
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jules Chevalier`,
+        short_name: `julechevalier`,
+        description: `Jules Chevalier is a designer & engineer who writes about emotional wellness, intentional living and inclusion.`,
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.png' // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        // crossOrigin: `use-credentials`,
+      }
+    }
   ]
 }
